62900c657ac6ad804fb0c3a2c77cd96b485f0145
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2024-03-21 22:52:27 +0100

    Header

--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -9,7 +9,7 @@ const variants = {
 };
 
 export default function Template({ children }) {
-	const completition = useScrollProgress();
+	const completion = useScrollProgress();
 	return (
 		<>
 			<motion.main
@@ -21,7 +21,7 @@ export default function Template({ children }) {
 				{children}
 			</motion.main>
 			<span
-				style={{ transform: `translateY(${completition - 100}%)` }}
+				style={{ transform: `translateY(${completion - 100}%)` }}
 				className="fixed z-50 bg-primary w-1 top-0 right-0 bottom-0 transition-all duration-700"
 			></span>
 			<div className="h-[4000px]"></div>
