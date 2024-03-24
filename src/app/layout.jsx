7b0eb87f86faf77482b44ee93b1004858a89d1eb
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2024-03-24 10:43:09 +0100

    Fix import issue in the header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ThemeProvider } from "@/components/ThemeProvider/ThemeProvider";
-import Template from "@/components/Template";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { ThemeProvider } from "../components/ThemeProvider/ThemeProvider";
+import Template from "../components/Template";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import useScrollProgress from "@/hooks/useScrollProgress";
+import useScrollProgress from "../../hooks/useScrollProgress";
 
 const variants = {
 	hidden: { opacity: 0 },
