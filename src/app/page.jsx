2d39111895c6e92fcf330880d7c6c6ca5ff5d474
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2024-04-01 22:16:21 +0200

    add services section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import Template from "../components/Template";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Services from "../components/Services";
 
 export default function Home() {
 	return (
 		<main>
 			<Hero />
 			<About />
+			<Services />
 		</main>
 	);
 }
