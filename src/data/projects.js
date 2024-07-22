b6841b55dcf2ced9260c537f458f1d4f75849b6f
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2024-07-22 22:29:12 +0200

    add subCategory field and exercise projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,29 @@
 export const projectData = [
 	{
+		image: "/assets/work/todolist.png",
+		category: "ReactJS",
+		subCategory: "Exercise Unfinished",
+		name: "To-Do List",
+		description:
+			"Unfinished To-Do List project, the goal is to work on CRUD operations",
+		link: "https://todolist-mathias.netlify.app/",
+		github: "https://github.com/mathias-grdl/Todo-List",
+		id: 7,
+	},
+	{
+		image: "/assets/work/movies-finder.png",
+		category: "ReactJS",
+		subCategory: "Exercise",
+		name: "Movies finder",
+		description: "Working with useEffects and fetching with useSWR",
+		link: "https://movie-search-hub.netlify.app/?s=Jumanji",
+		github: "https://github.com/mathias-grdl/BeginReact-Workshop-UseEffect",
+		id: 6,
+	},
+	{
 		image: "/assets/work/portfolio.png",
 		category: "ReactJS",
+		subCategory: "",
 		name: "My Portfolio",
 		description:
 			"My portfolio using: React.js, Next.js 14, Tailwind CSS, Shadcn UI",
@@ -12,6 +34,7 @@ export const projectData = [
 	{
 		image: "/assets/work/obandito-home.png",
 		category: "ReactJS",
+		subCategory: "",
 		name: "O'bandito",
 		description:
 			"Project O'clock with 3 people, using ReactJS/Redux, GraphQL",
@@ -22,6 +45,7 @@ export const projectData = [
 	{
 		image: "/assets/work/maison-thishan.png",
 		category: "Wordpress",
+		subCategory: "",
 		name: "Maison Thishan",
 		description:
 			"Creating an e-commerce website for an independent perfumer",
@@ -32,6 +56,7 @@ export const projectData = [
 	{
 		image: "/assets/work/tp66.png",
 		category: "Wordpress",
+		subCategory: "",
 		name: "TP66",
 		description:
 			"Redesign of the website for the largest construction company in the Eastern Pyrenees",
@@ -42,6 +67,7 @@ export const projectData = [
 	{
 		image: "/assets/work/kasa.png",
 		category: "ReactJS",
+		subCategory: "",
 		name: "Kasa",
 		description:
 			"Kasa is my first ReactJS project aimed at showcasing different apartments",
